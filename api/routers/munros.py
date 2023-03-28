99063be8425f9a0dad5ebf7c64f56e16b1073039
Elijah <elijahbramos@gmail.com>
2023-03-28 15:04:11 -0700

    feat: require login for list munros

--- api/routers/munros.py
+++ api/routers/munros.py
@@ -3,12 +3,16 @@ from models import MunrosList
 from queries.munros import MunrosQueries
 from typing import List
 from models import MunroParams
+from authenticator import authenticator
 
 router = APIRouter()
 
 
 @router.get("/api/munros", response_model=MunrosList)
-def get_all_munros(munros: MunrosQueries = Depends()):
+def get_all_munros(
+    munros: MunrosQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data)
+    ):
     return {"munros": munros.get_all()}
 
 
