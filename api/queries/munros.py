daf471c0c1cf01c654dbe540b19cc17e05257044
Brian Rabern <brian.rabern@gmail.com>
2023-03-29 00:04:04 +0100

    get all, get one munro

--- api/queries/munros.py
+++ api/queries/munros.py
@@ -1,6 +1,8 @@
-import csv
 from queries.client import Queries
+
+# import csv
 from models import Munro, MunroParams
+from bson import ObjectId
 
 # from bson.objectid import ObjectId
 from typing import List
@@ -16,22 +18,34 @@ class MunrosQueries(Queries):
             munros.append(Munro(**munro))
         return munros
 
-    def seed_database(self, params_list: List[MunroParams]) -> List[Munro]:
+    def get_one(self, munro_id: str) -> Munro:
+        munro = self.collection.find_one({"_id": ObjectId(munro_id)})
+        munro["id"] = str(munro["_id"])
+        return Munro(**munro)
 
-        csvFile = open("munroData.csv", "r")
-        munro_reader = csv.DictReader(csvFile)
+    # def get(self, username: str):
+    #     result = self.collection.find_one({"username": username})
+    #     if result is None:
+    #         return None
+    #     result["id"] = str(result["_id"])
+    #     return AccountOutWithPassword(**result)
 
-        munro_list = []
+    # def seed_database(self, params_list: List[MunroParams]) -> List[Munro]:
 
-        for dct in map(dict, munro_reader):
-            clean_dct = {k.strip(): v.strip() for k, v in dct.items()}
-            clean_dct["reviews"] = []
-            munro_list.append(clean_dct)
+    #     csvFile = open("munroData.csv", "r")
+    #     munro_reader = csv.DictReader(csvFile)
 
-        result = self.collection.insert_many(munro_list)
-        inserted_ids = [str(id_) for id_ in result.inserted_ids]
+    #     munro_list = []
 
-        x = len(result.inserted_ids)
-        print(f"Guid yin! Ye've addit {x} munros tae yer croun!")
+    #     for dct in map(dict, munro_reader):
+    #         clean_dct = {k.strip(): v.strip() for k, v in dct.items()}
+    #         clean_dct["reviews"] = []
+    #         munro_list.append(clean_dct)
 
-        return inserted_ids
+    #     result = self.collection.insert_many(munro_list)
+    #     inserted_ids = [str(id_) for id_ in result.inserted_ids]
+
+    #     x = len(result.inserted_ids)
+    #     print(f"Guid yin! Ye've addit {x} munros tae yer croun!")
+
+    #     return inserted_ids
