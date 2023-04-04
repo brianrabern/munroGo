fbad5921159ce667efe0e2487ea31d69fd702328
Elijah <elijahbramos@gmail.com>
2023-04-04 11:54:45 -0700

    feat: reviews for account, auth on all climbs for munro

--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -15,3 +15,13 @@ def get_all_reviews_by_munro(
     account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     return {"reviews": reviews.get_all_by_munro(munro_id)}
+
+
+@router.get("/api/munros/account/reviews/", response_model=ReviewsList)
+def get_reviews_for_account(
+    reviews: ReviewsQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    account_id = str(account_data["id"])
+    reviews = reviews.get_all_by_account(account_id)
+    return {"reviews": reviews}
