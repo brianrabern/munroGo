f142cfd0bf7c488759f8c9f1e962c570045f2e95
Warren Hill <mr.wrnhill@gmail.com>
2023-04-04 15:27:21 -0500

    Create Review done

--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -25,3 +25,20 @@ def get_reviews_for_account(
     account_id = str(account_data["id"])
     reviews = reviews.get_all_by_account(account_id)
     return {"reviews": reviews}
+
+
+@router.post("/api/munros/{munro_id}/reviews/", response_model=Review, tags=["reviews"])
+def create_review(
+    munro_id: str,
+    comment: str,
+    rating: int,
+    reviews: ReviewsQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    params = {
+              "munro_id": munro_id,
+              "account_id": str(account_data["id"]),
+              "comment": comment,
+              "rating": rating,
+              }
+    return reviews.create_one(params)
