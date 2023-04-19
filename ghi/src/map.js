1c01c50396b4a0df3298f579bf0a9a8d79c5174d
Elijah <elijahbramos@gmail.com>
2023-04-19 16:35:26 -0500

    feat: making progress on map

--- ghi/src/map.js
+++ ghi/src/map.js
@@ -31,7 +31,6 @@ function Map() {
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
 
     setMap(map);
   }, []);
@@ -44,7 +43,7 @@ function Map() {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={6}
+      zoom={8}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
