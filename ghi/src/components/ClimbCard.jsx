f164a9e017c89fb8703cac826f1227a45e0ec2f5
Brian Rabern <brian.rabern@gmail.com>
2023-04-25 08:45:10 +0100

    start

--- ghi/src/components/ClimbCard.jsx
+++ ghi/src/components/ClimbCard.jsx
@@ -9,7 +9,7 @@ export default function ClimbCard({ climb }) {
   });
   const munro = munros.find((munro) => munro.id === climb.munro_id);
   return (
-    <div className="card w-96 bg-base-300 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title">{munro.hillname}</h2>
         <p>{date}</p>
@@ -18,6 +18,62 @@ export default function ClimbCard({ climb }) {
         <p>Notes: {climb.notes}</p>
         <p>Weather: {climb.weather}</p>
       </div>
+      <figure>
+        <img
+          src={
+            climb.image
+              ? `data:image/png;base64,${climb.image}`
+              : "https://brianrabern.net/munro.png"
+          }
+          alt=""
+          key={climb.id}
+          style={{ maxHeight: "100px" }}
+        />
+      </figure>
     </div>
+
+    // <div className="card w-96 bg-base-100 shadow-xl">
+    //   <figure className="px-10 pt-10">
+    //     <img
+    //       src={
+    //         climb.image
+    //           ? `data:image/png;base64,${climb.image}`
+    //           : "https://brianrabern.net/munro.png"
+    //       }
+    //       alt=""
+    //       key={climb.id}
+    //       className="rounded-xl"
+    //     />
+    //   </figure>
+    //   <div className="card-body items-center text-center">
+    //     <h2 className="card-title">{munro.hillname}</h2>
+    //     <p>{date}</p>
+    //     <p>Difficulty: {climb.difficulty}</p>
+    //     <p>Duration: {climb.duration}</p>
+    //     <p>Notes: {climb.notes}</p>
+    //     <p>Weather: {climb.weather}</p>
+    //   </div>
+    // </div>
   );
+}
+
+{
+  /* <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img
+      src={`data:image/png;base64,${climb.image}`}
+      alt=""
+      key={climb.id}
+      className="rounded-xl"
+    />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{munro.hillname}</h2>
+    <p>{date}</p>
+    <p>Difficulty: {climb.difficulty}</p>
+    <p>Duration: {climb.duration}</p>
+    <p>Notes: {climb.notes}</p>
+    <p>Weather: {climb.weather}</p>
+  </div>
+</div>; */
 }
