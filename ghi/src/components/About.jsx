75662d5a8632692961f4de56097b71952d71349a
Diganta Roy <roydiganta_34@yahoo.com>
2023-05-11 15:41:56 -0400

    round 3

--- ghi/src/components/About.jsx
+++ ghi/src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AiFillGithub, AiFillGitlab, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGitlab, AiFillLinkedin } from "react-icons/ai";
 import {
   SiPython,
   SiJavascript,
