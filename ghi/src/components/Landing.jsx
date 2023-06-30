045379e774f350aa425490666c5b12e5e9a95380
Diganta Roy <roydiganta_34@yahoo.com>
2023-06-30 00:55:08 -0400

    start 2

--- ghi/src/components/Landing.jsx
+++ ghi/src/components/Landing.jsx
@@ -1,3 +1,10 @@
-const Landing = () => {
-  
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ImageSlider from "./ImageSlider";
+import { SliderData } from "./SliderData.jsx";
+
+function Landing() {
+
 }
+
+export Landing
